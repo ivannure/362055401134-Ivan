e598888567e8f8ff0783e0ad73eda3994ec0f18e
ivannure <100396470+ivannure@users.noreply.github.com>
2022-02-25 02:09:18 -0800

    Update page2.dart

--- lib/page2.dart
+++ lib/page2.dart
@@ -1,60 +1,182 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "UTS",
-    home: menudata(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(MyApp());
 }
 
-class menudata extends StatefulWidget {
-  @override
-  _menudataState createState() => _menudataState();
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: home(),
+    );
+  }
 }
 
-class _menudataState extends State<menudata> {
-  final _formKey = GlobalKey<FormState>();
+class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      width: double.infinity,
-      color: Colors.green,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Padding(padding: EdgeInsets.all(20)),
-          Icon(
-            Icons.person,
-            size: 90,
-          ),
-          BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                title: Text('Beranda'),
+      backgroundColor: Colors.green,
+      body: Center(
+        child: Column(
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 80),
+                  CircleAvatar(
+                    backgroundColor: Colors.black,
+                    maxRadius: 30,
+                    child: Icon(Icons.person),
+                  ),
+                  Text(
+                    'Ivan Nur Efendi',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.white),
+                  ),
+                  Text(
+                    'NIM: 362055401134',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.notifications),
-                title: Text('Notifikasi'),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20))),
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      'Menu Utama',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.help),
-                title: Text('About'),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                title: Text('Akun'),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [Icon(Icons.book), Text('Jurusan')],
+                  ),
+                  Column(
+                    children: [Icon(Icons.person), Text('Identitas')],
+                  ),
+                  Column(
+                    children: [Icon(Icons.note), Text('Kartu Peserta')],
+                  ),
+                  Column(
+                    children: [Icon(Icons.note_add), Text('Slip Pembayaran')],
+                  ),
+                ],
               ),
-            ],
-            currentIndex: 0,
-            selectedItemColor: Colors.green,
-            unselectedItemColor: Colors.grey,
-            showUnselectedLabels: true,
-          ),
-        ],
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+              ),
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      'Informasi terbaru',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 380,
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+              ),
+              child: ListView(
+                children: [
+                  Card(
+                    margin: EdgeInsets.all(15),
+                    color: Color.fromRGBO(104, 164, 136, 100),
+                    shadowColor: Colors.black,
+                    child: ListTile(
+                        contentPadding: EdgeInsets.all(20),
+                        title: Text('Informasi SBMPTN',
+                            style: TextStyle(fontSize: 16)),
+                        subtitle: Text('Dapatkan Informasi SBMPTN Disini'),
+                        leading: CircleAvatar(
+                            backgroundColor: Colors.green,
+                            child: Icon(Icons.notifications))),
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(15),
+                    color: Color.fromRGBO(104, 164, 136, 100),
+                    shadowColor: Colors.black,
+                    child: ListTile(
+                        contentPadding: EdgeInsets.all(20),
+                        title: Text('Jadwal SBMPTN',
+                            style: TextStyle(fontSize: 16)),
+                        subtitle: Text('Dapatkan Jadwal SBMPTN Disini'),
+                        leading: CircleAvatar(
+                            backgroundColor: Colors.green,
+                            child: Icon(Icons.notifications))),
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(15),
+                    color: Color.fromRGBO(104, 164, 136, 100),
+                    shadowColor: Colors.black,
+                    child: ListTile(
+                        contentPadding: EdgeInsets.all(20),
+                        title: Text('Informasi SBMPTN',
+                            style: TextStyle(fontSize: 16)),
+                        subtitle: Text('Dapatkan Informasi SBMPTN Disini'),
+                        leading: CircleAvatar(
+                            backgroundColor: Colors.green,
+                            child: Icon(Icons.notifications))),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
+
+class note extends StatelessWidget {
+  const note({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(15),
+      color: Colors.white,
+      shadowColor: Colors.black,
+      child: ListTile(
+          contentPadding: EdgeInsets.all(20),
+          title: Text('Cetak Kartu Peserta', style: TextStyle(fontSize: 16)),
+          subtitle: Text('Silahkan Unduh Kartu Peserta Anda'),
+          leading: CircleAvatar(
+              backgroundColor: Colors.green, child: Icon(Icons.notifications))),
+    );
+  }
+}
+
